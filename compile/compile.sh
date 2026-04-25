e3c30c0abe6e3b21c6cc6726d516c35394287522
DVP-F <carnx@duck.com>
2026-04-25 10:18:20 +0100

    way more unified

--- compile/compile.sh
+++ compile/compile.sh
@@ -8,9 +8,8 @@ help_print() {
 Usage:
 $0 [-h|--help] <Compiler> <Package manager>
 
-    -h, --help
-        Print this help message
-
+-h, --help
+    Print this help message
 <Compiler>:
     gcc, g++, cc, clang
 <Package manager>:
@@ -20,9 +19,9 @@ Exit codes:
 1 - Unexpected error
 2 - Help message
 3 - Incorrect syntax
-4 - Package manager required and not given
+    (Missing/unknown arguments)
 
-Check return codes with:
+Check exit codes with:
 \`echo \$?\`
 \`echo \$PIPESTATUS\` (bash exclusive)
 EOF
@@ -30,35 +29,39 @@ EOF
 }
 
 # Sanitize arguments and deal with help arg
-if [[ $# -ne 2 ]]; then 
-    echo "Error: expected 1 (one) to 2 (two) arguments." >&2
-    help_print
-fi
-case "$1" in 
-    -h|--help)
+handle_args() {
+    # idc enough to use getopt, sue me
+    if [[ $# -gt 2 && $# -eq 0 ]]; then 
+        echo "Error: expected 1 (one) to 2 (two) arguments." >&2
         help_print
-        ;;
-    gcc|g++|cc|clang)
-        echo "Using compiler '$1'"
-        ;;
-    *)
-        echo "Unknown compiler: '$1'" >&2
-        exit 3
-        ;;
-esac
-if [[ $# -eq 2 ]]; then
-    case "$2" in
-        apt|apt-get|pacman)
-            echo "Using package manager '$2'"
+    fi
+    case "$1" in 
+        -h|--help)
+            help_print
+            ;;
+        gcc|g++|cc|clang)
+            echo "Using compiler '$1'"
             ;;
         *)
-            echo "Unknown package manager: '$2'" >&2
+            echo "Unknown compiler: '$1'" >&2
             exit 3
             ;;
     esac
-else
-    echo "No package manager given; Will not automatically install"
-fi
+    if [[ $# -eq 2 ]]; then
+        case "$2" in
+            apt|apt-get|pacman)
+                echo "Using package manager '$2'"
+                ;;
+            *)
+                echo "Unknown package manager: '$2'" >&2
+                exit 3
+                ;;
+        esac
+    else
+        echo "No package manager given; Aborting" >&2
+        exit 3
+    fi
+}
 
 # functions to install mingw if not installed
 install_mingw_gcc() {
@@ -89,6 +92,7 @@ install_mingw_gcc() {
     else
         echo "No package manager given, aborting." >&2
         exit 4
+    fi
 }
 install_mingw_gpp() {
     echo "g++ equivalent Mingw64 not found, attempting install."
@@ -162,36 +166,41 @@ compile_clang() {
     x86_64-w64-mingw32-clang++ -std=c++17 -o ../bin/DDCL.exe ../source/DDCL.cpp -lpsapi -lws2_32 -lwininet -liphlpapi -static-libgcc -static-libstdc++
 }
 
-# check that compiler exists and is available
-# if it is, compile
-# if it isnt, try to install it.
-case "$1" in
-    gcc|cc)
-        if command -v x86_64-w64-mingw32-gcc >/dev/null 2>&1; then
-            compile_gcc
-        # if not, check with pkgman as well as if it can be installed (if defined)
-        else
-            install_mingw_gcc && compile_gcc
-        fi
-        ;;
-    g++)
-        if command -v x86_64-w64-mingw32-g++ >/dev/null 2>&1; then
-            compile_gpp
-        # if not, check with pkgman as well as if it can be installed (if defined)
-        else
-            install_mingw_gpp && compile_gpp
-        fi
-        ;;
-    clang)
-        if command -v x86_64-w64-mingw32-clang++ >/dev/null 2>&1; then
-            compile_clang
-        # if not, check with pkgman as well as if it can be installed (if defined)
-        else
-            install_mingw_clang && compile_clang
-        fi
-        ;;
-esac
+try_compile() {
+    # check that compiler exists and is available
+    # if it is, compile
+    # if it isnt, try to install it.
+    case "$1" in
+        gcc|cc)
+            if command -v x86_64-w64-mingw32-gcc >/dev/null 2>&1; then
+                compile_gcc
+            # if not, check with pkgman as well as if it can be installed (if defined)
+            else
+                install_mingw_gcc && compile_gcc
+            fi
+            ;;
+        g++)
+            if command -v x86_64-w64-mingw32-g++ >/dev/null 2>&1; then
+                compile_gpp
+            # if not, check with pkgman as well as if it can be installed (if defined)
+            else
+                install_mingw_gpp && compile_gpp
+            fi
+            ;;
+        clang)
+            if command -v x86_64-w64-mingw32-clang++ >/dev/null 2>&1; then
+                compile_clang
+            # if not, check with pkgman as well as if it can be installed (if defined)
+            else
+                install_mingw_clang && compile_clang
+            fi
+            ;;
+        # Already sanitized the contents of $1 so no default here
+    esac
+}
 
-
+# pass all args along to the handlers
+handle_args $@
+try_compile $@
 
 # export WINEDEUBG=+dll ; wine ../bin/DDCL.exe
