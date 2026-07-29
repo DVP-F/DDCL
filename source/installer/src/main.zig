60731ae6230ce45d8b3adb82582a5a4fbdd4d0f9
DVP-F <carnx@duck.com>
2026-07-29 10:42:22 +0200

    slightly jank zig

--- source/installer/src/main.zig
+++ source/installer/src/main.zig
@@ -3,69 +3,108 @@ const Io = std.Io;
 
 const installer = @import("installer");
 
-pub fn main(init: std.process.Init) !void {
-    // Prints to stderr, unbuffered, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+//? real quick: im aware that std.fs.cwd() use is deprecated since 0.16 zig. IDC tho!
+//? im not gonna make this more complicated for myself
 
-    // This is appropriate for anything that lives as long as the process.
-    const arena: std.mem.Allocator = init.arena.allocator();
+//* if i need a temp allocator:
+// var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+// defer arena.deinit();
+// const allocator: std.mem.Allocator = arena.allocator();
 
-    // Accessing command line arguments:
-    const args = try init.minimal.args.toSlice(arena);
-    for (args) |arg| {
-        std.log.info("arg: {s}", .{arg});
+fn getInstallDir(arena: std.mem.Allocator) ![]u8 {
+    // current directory is plausible
+    var installDir: []u8 = try std.process.getEnvVarOwned(arena, "cd");
+    defer arena.free(installDir);
+    // see if executable is here
+    if (
+        //? this check is only performed twice so im not bothering to make it its own fn
+        try std.fs.cwd().access(
+            try std.fs.path.join(arena, &.{
+                installDir,
+                "DDCL.exe"
+            }), .{}) catch |err| switch (err) {
+            error.FileNotFound => {
+                std.debug.print("File does not exist\n", .{});
+                return;
+            }, else => return err,
+        }
+    ) {
+        return installDir;
+    } {
+        // else try the default install path of the MSI
+        installDir = "C:\\Program Files\\DDCL";
+        if (
+            try std.fs.cwd().access(
+                try std.fs.path.join(arena, &.{
+                    installDir,
+                    "DDCL.exe"
+                }), .{}) catch |err| switch (err) {
+                error.FileNotFound => {
+                    std.debug.print("File does not exist\n", .{});
+                    return;
+                }, else => return err,
+            }
+        ) {
+            return installDir;
+        }
     }
+    // couldnt verify dir
+    return error.FileNotFound;
+}
 
-    // In order to do I/O operations need an `Io` instance.
-    const io = init.io;
+fn _doAddShortcut(arena: std.mem.Allocator, installDir: std.fs.path) !void {
+    //* let errors propagate out
+    // first off create the directory
+    try std.fs.cwd().makePath("C:\\ProgramData\\Microsoft\\Windows\\tart Menu\\Programs\\DDCL");
+    // VBS script to generate a .lnk
+    // multiline strings dont expand escapes after 0.16
+    const scriptText: []u8 = try std.fmt.allocPrint(
+        arena,
+        \\Set WshShell = CreateObject("WScript.Shell")
+        \\Set oLink = WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\DDCL\DDCL.lnk")
+        \\oLink.TargetPath = "{s}\DDCL.exe"
+        \\oLink.WorkingDirectory = "{s}"
+        \\oLink.Save
+        , .{ installDir, installDir },
+    );
+    defer arena.free(scriptText);
+    // write to a temp file
+    const spath = try std.fs.path.join(arena, &.{
+        try std.process.getEnvVarOwned(arena, "TEMP"),
+        "t_script.vbs" });
+    defer arena.free(spath);
+    try std.fs.cwd().writeFile(.{
+        .sub_path = spath,
+        .data = scriptText,
+    });
+    // then run cscript on it
+    var child = std.process.Child.init(
+        &[_][]const u8{ "cscript", "//NoLogo", spath, },
+        std.heap.page_allocator,
+    );
+    _ = try child.spawnAndWait();
+    // then delete the script file
+    try std.fs.cwd().deleteFile(spath);
+}
 
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
+fn addShortcut(arena: std.mem.Allocator, installDir: std.fs.path) bool {
+    _doAddShortcut(arena, installDir) catch |err| {
+        // still return a bool on failure
+        std.debug.print("Error: {}\n", .{err});
+        return false;
+    };
+    // if everything worked:
+    return true;
+}
+
+pub fn main(init: std.process.Init) !void {
+    const arena: std.mem.Allocator = init.arena.allocator();
+    const io: Io = init.io;
     var stdout_buffer: [1024]u8 = undefined;
     var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
     const stdout_writer = &stdout_file_writer.interface;
 
-    try installer.printAnotherMessage(stdout_writer);
+    const lnkStatus: bool = try addShortcut(arena, );
 
     try stdout_writer.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    try std.testing.fuzz({}, testOne, .{});
-}
-
-fn testOne(context: void, smith: *std.testing.Smith) !void {
-    _ = context;
-    // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(u8) = .empty;
-    defer list.deinit(gpa);
-    while (!smith.eos()) switch (smith.value(enum { add_data, dup_data })) {
-        .add_data => {
-            const slice = try list.addManyAsSlice(gpa, smith.value(u4));
-            smith.bytes(slice);
-        },
-        .dup_data => {
-            if (list.items.len == 0) continue;
-            if (list.items.len > std.math.maxInt(u32)) return error.SkipZigTest;
-            const len = smith.valueRangeAtMost(u32, 1, @min(32, list.items.len));
-            const off = smith.valueRangeAtMost(u32, 0, @intCast(list.items.len - len));
-            try list.appendSlice(gpa, list.items[off..][0..len]);
-            try std.testing.expectEqualSlices(
-                u8,
-                list.items[off..][0..len],
-                list.items[list.items.len - len ..],
-            );
-        },
-    };
 }
