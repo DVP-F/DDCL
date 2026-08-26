fb86c1dc92a4d96f7258b3c7042a84403f6d91b5
DVP-F <carnx@duck.com>
2026-08-26 19:21:10 +0200

    ensure cross-compilation works

--- source/installer/build.zig
+++ source/installer/build.zig
@@ -17,7 +17,11 @@ pub fn build(b: *std.Build) void {
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
     // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
+    const target = b.resolveTargetQuery(.{
+        .cpu_arch = .x86_64,
+        .os_tag = .windows,
+        .abi = .gnu,
+    });
     // Standard optimization options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
